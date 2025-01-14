0c1cfa70b8f8b6c01c09aa28c3c3d73e4de66643
TandyB0Bandy <jakubgorgon1@gmail.com>
2025-01-14 18:25:21 +0100

    added simple error handling

--- app.py
+++ app.py
@@ -5,245 +5,248 @@ import plotly.express as px
 from wordcloud import WordCloud, STOPWORDS
 
 # st.set_page_config(layout="wide")
-@st.cache_data(persist=False)
-def load_data():
-    df_raw = pd.read_pickle("data/interim/tweets_processed.pkl")
-    df = pd.read_pickle("data/interim/tweets_no_nas.pkl")
-    return df_raw, df
 
-df_raw, df = load_data()
+try:
+    @st.cache_data(persist=False)
+    def load_data():
+        df_raw = pd.read_pickle("data/interim/tweets_processed.pkl")
+        df = pd.read_pickle("data/interim/tweets_no_nas.pkl")
+        return df_raw, df
 
-
-st.markdown(
-    """
-    <style>
-    .title {
-        font-size: 50px;
-        font-weight: bold;
-        color: #1f77b4;
-        text-align: center;
-        text-shadow: 2px 2px #ffffff;
-    }
-    .emoji {
-        font-size: 40px;
-    }
-    </style>
-    <p class="title">Sentiment Analysis of Tweets About US Airlines <span class="emoji">🐦</span></p>
-    """,
-    unsafe_allow_html=True
-)
+    df_raw, df = load_data()
 
 
+    st.markdown(
+        """
+        <style>
+        .title {
+            font-size: 50px;
+            font-weight: bold;
+            color: #1f77b4;
+            text-align: center;
+            text-shadow: 2px 2px #ffffff;
+        }
+        .emoji {
+            font-size: 40px;
+        }
+        </style>
+        <p class="title">Sentiment Analysis of Tweets About US Airlines <span class="emoji">🐦</span></p>
+        """,
+        unsafe_allow_html=True
+    )
 
-st.sidebar.subheader("Example Random Tweet")
-sentiments = tuple(df['airline_sentiment'].unique())
-sentiment = st.sidebar.selectbox(label="Sentiment", 
-                                 options=sentiments, 
-                                 placeholder="Choose to See an Example",
-                                 key='s')
 
-# Check if the sentiment is not in session state
-if sentiment not in st.session_state:
-    filt = df['airline_sentiment'] == sentiment
-    df_filt = df[filt]
 
-    # Use the retweet_count column as weights for sampling
-    random_tweet = df_filt[['text', 'retweet_count']].sample(
-        weights=df_filt['retweet_count'] + 1,  # Add 1 to avoid zero weights
-        n=1
-    ).iloc[0, 0:2]
-    
-    st.session_state[sentiment] = [
-        random_tweet.iloc[0],  # Tweet text
-        str(random_tweet.iloc[1])  # Retweet count
-    ]
+    st.sidebar.subheader("Example Random Tweet")
+    sentiments = tuple(df['airline_sentiment'].unique())
+    sentiment = st.sidebar.selectbox(label="Sentiment", 
+                                    options=sentiments, 
+                                    placeholder="Choose to See an Example",
+                                    key='s')
 
-# Concatenate tweet text and retweet count
-tweet_text = st.session_state[sentiment][0]
-retweet_count = st.session_state[sentiment][1]
-formatted_tweet = f"Tweet: {tweet_text}\n\nRetweets: {retweet_count}"
+    # Check if the sentiment is not in session state
+    if sentiment not in st.session_state:
+        filt = df['airline_sentiment'] == sentiment
+        df_filt = df[filt]
 
-# Display the formatted tweet in the sidebar
-st.sidebar.write(formatted_tweet)
+        # Use the retweet_count column as weights for sampling
+        random_tweet = df_filt[['text', 'retweet_count']].sample(
+            weights=df_filt['retweet_count'] + 1,  # Add 1 to avoid zero weights
+            n=1
+        ).iloc[0, 0:2]
+        
+        st.session_state[sentiment] = [
+            random_tweet.iloc[0],  # Tweet text
+            str(random_tweet.iloc[1])  # Retweet count
+        ]
 
-chart_options = ("Bar Chart", "Pie Chart")
-st.sidebar.subheader("Visualise Distribution of Tweets by Sentiment")
-plots = st.sidebar.selectbox("Visualisation Type", 
-                               chart_options,
-                               key='p')
+    # Concatenate tweet text and retweet count
+    tweet_text = st.session_state[sentiment][0]
+    retweet_count = st.session_state[sentiment][1]
+    formatted_tweet = f"Tweet: {tweet_text}\n\nRetweets: {retweet_count}"
 
-sentiment_series = df['airline_sentiment'].value_counts()
-names = sentiment_series.index
-counts = sentiment_series.values
+    # Display the formatted tweet in the sidebar
+    st.sidebar.write(formatted_tweet)
 
-custom_colors = ['#EF553B', '#636EFA', '#00CC96'] 
+    chart_options = ("Bar Chart", "Pie Chart")
+    st.sidebar.subheader("Visualise Distribution of Tweets by Sentiment")
+    plots = st.sidebar.selectbox("Visualisation Type", 
+                                chart_options,
+                                key='p')
 
-if st.sidebar.checkbox("Show Chart", False):
-    st.subheader("Distribution of Tweets by Sentiment")
-    if "Pie Chart" in plots:
-        fig = px.pie(data_frame=sentiment_series,
-                    values=counts,
-                    names=names,
-                    color_discrete_sequence=custom_colors,
-                    labels={"airline_sentiment":"Tweet Sentiment"}
-                    )
-        # st.subheader("Distribution of Tweets by Sentiment")
+    sentiment_series = df['airline_sentiment'].value_counts()
+    names = sentiment_series.index
+    counts = sentiment_series.values
+
+    custom_colors = ['#EF553B', '#636EFA', '#00CC96'] 
+
+    if st.sidebar.checkbox("Show Chart", False):
+        st.subheader("Distribution of Tweets by Sentiment")
+        if "Pie Chart" in plots:
+            fig = px.pie(data_frame=sentiment_series,
+                        values=counts,
+                        names=names,
+                        color_discrete_sequence=custom_colors,
+                        labels={"airline_sentiment":"Tweet Sentiment"}
+                        )
+            # st.subheader("Distribution of Tweets by Sentiment")
+            st.plotly_chart(fig)
+
+        if "Bar Chart" in plots:
+            fig = px.bar(data_frame=sentiment_series,
+                        y = counts,
+                        x = names,
+                        color=names,
+                        color_discrete_sequence=custom_colors,
+                        labels={'airline_sentiment': 'Tweet Sentiment', 
+                                'y': 'Number of Tweets'}
+                        )
+            st.plotly_chart(fig)
+
+    st.sidebar.subheader("When and Where Are Users Tweeting From")
+
+    hours = st.sidebar.slider("Hour of Day", 0, 24, value = (12, 13))
+
+    df['hour_tweet'] = df['tweet_created'].dt.hour
+
+    # hours = list(range(0,24))
+    # for h in hours:
+    #     if hour == h:
+    #         filt = df['hour_tweet'] == h
+    #         df_filt = df[filt]
+            
+    #         st.subheader(f"Map of Tweets Sent at {hour}")
+    #         st.map(df_filt)
+
+    # df['hour_tweet'] > hour[0] & (df['hour_tweet'] < hour[1])
+        
+    def plot_map():
+        filt = (df['hour_tweet'] >= hours[0]) & (df['hour_tweet'] < hours[1])
+        df_filt = df[filt]
+        st.subheader(f"Tweets sent Between {hours[0]}:00 and {hours[1]}:00")
+        st.markdown(f"* {df_filt.shape[0]} total tweets")
+        
+        negative_ratio = round(df_filt['airline_sentiment'].value_counts(normalize=True)[0], 2)*100
+        st.markdown(f"* {negative_ratio}% of tweets were negative")
+
+        st.map(df_filt)
+
+    if st.sidebar.checkbox("Show Map", False):
+        plot_map()
+
+    df.groupby(by='airline')['airline_sentiment'].value_counts(normalize=True)
+
+    st.sidebar.subheader("Break Down Airline Tweets by Sentiment")
+
+    airlines = df['airline'].unique()
+    choices = st.sidebar.multiselect("Airlines", options = airlines,
+                        placeholder = "Choose Airlines to Compare")
+
+
+    sep = ", "
+
+    if len(choices) > 0:
+        st.subheader(f"Tweet Sentiment by Airline (including {sep.join(choices)})")
+
+        filt = df['airline'].isin(choices)
+        df_filt = df[filt]
+        grouped = pd.DataFrame(df_filt.groupby(by='airline')['airline_sentiment'].value_counts(normalize=True)*100)
+        grouped.reset_index(inplace=True)
+        grouped['proportion'] = round(grouped['proportion'], 2)
+            
+        insights_filt_pos = grouped['airline_sentiment'] == "positive"
+        df_insights = grouped[insights_filt_pos]
+        filt = df_insights['proportion'] == df_insights['proportion'].max()
+        name = df_insights[filt]["airline"].iloc[0]
+        value_pos = round(df_insights[filt]["proportion"].iloc[0], 2)
+
+        insights_filt_neg = grouped['airline_sentiment'] == "negative"
+        df_insights_neg = grouped[insights_filt_neg]
+        filt_neg = df_insights_neg['proportion'] == df_insights_neg['proportion'].max()
+        name_neg = df_insights_neg[filt_neg]["airline"].iloc[0]
+        value_neg = round(df_insights_neg[filt_neg]["proportion"].iloc[0], 2)
+
+        st.markdown(f"* {name_neg} has the highest proportion of negative tweets ({value_neg} %)")
+        st.markdown(f"* {name} has the highest proportion of positive tweets ({value_pos} %)")
+
+
+        fig = px.bar(data_frame =grouped, 
+                        x='airline', y='proportion',
+                        
+                        facet_col='airline_sentiment',
+                        color='airline',
+                        labels={"airline_sentiment":"Tweets", 
+                        "proportion": "Proportion of Tweets (%)"})
         st.plotly_chart(fig)
 
-    if "Bar Chart" in plots:
-        fig = px.bar(data_frame=sentiment_series,
-                    y = counts,
-                    x = names,
-                    color=names,
-                    color_discrete_sequence=custom_colors,
-                    labels={'airline_sentiment': 'Tweet Sentiment', 
-                            'y': 'Number of Tweets'}
-                    )
-        st.plotly_chart(fig)
+    st.sidebar.subheader("Word Cloud for Tweets")
+    sentiment_radio = st.sidebar.radio("Choose Sentiment", 
+                    tuple(df['airline_sentiment'].unique()))
 
-st.sidebar.subheader("When and Where Are Users Tweeting From")
 
-hours = st.sidebar.slider("Hour of Day", 0, 24, value = (12, 13))
 
-df['hour_tweet'] = df['tweet_created'].dt.hour
+    # for word in words:
+    #     if 'http' in word or word.startswith("@") or word == 'RT':
+    #         words.remove(word)
 
-# hours = list(range(0,24))
-# for h in hours:
-#     if hour == h:
-#         filt = df['hour_tweet'] == h
-#         df_filt = df[filt]
+    @st.cache_data(persist=True)
+    def split_df_by_sentiment():
+        filt_neg = df['airline_sentiment'] == 'negative'
+        filt_neu = df['airline_sentiment'] == 'neutral'
+        filt_pos = df['airline_sentiment'] == 'positive'
+
+        df_neg = df[filt_neg]
+        df_neu = df[filt_neu]
+        df_pos = df[filt_pos]
+
+        return df_neg, df_neu, df_pos
+
+    df_neg, df_neu, df_pos = split_df_by_sentiment()
+
+    @st.cache_data(persist=True)
+    def create_word_lists():
+        sep = " "
         
-#         st.subheader(f"Map of Tweets Sent at {hour}")
-#         st.map(df_filt)
+        text_list_neg = df_neg['text'].tolist()
+        text_list_neu = df_neu['text'].tolist()
+        text_list_pos = df_pos['text'].tolist()
 
-# df['hour_tweet'] > hour[0] & (df['hour_tweet'] < hour[1])
-    
-def plot_map():
-    filt = (df['hour_tweet'] >= hours[0]) & (df['hour_tweet'] < hours[1])
-    df_filt = df[filt]
-    st.subheader(f"Tweets sent Between {hours[0]}:00 and {hours[1]}:00")
-    st.markdown(f"* {df_filt.shape[0]} total tweets")
-    
-    negative_ratio = round(df_filt['airline_sentiment'].value_counts(normalize=True)[0], 2)*100
-    st.markdown(f"* {negative_ratio}% of tweets were negative")
+        words_neg = sep.join(text_list_neg).split()
+        words_neu = sep.join(text_list_neu).split()
+        words_pos = sep.join(text_list_pos).split()
 
-    st.map(df_filt)
-
-if st.sidebar.checkbox("Show Map", False):
-    plot_map()
-
-df.groupby(by='airline')['airline_sentiment'].value_counts(normalize=True)
-
-st.sidebar.subheader("Break Down Airline Tweets by Sentiment")
-
-airlines = df['airline'].unique()
-choices = st.sidebar.multiselect("Airlines", options = airlines,
-                    placeholder = "Choose Airlines to Compare")
+        lists_of_words = [words_neg, words_neu, words_pos]
 
 
-sep = ", "
-
-if len(choices) > 0:
-    st.subheader(f"Tweet Sentiment by Airline (including {sep.join(choices)})")
-
-    filt = df['airline'].isin(choices)
-    df_filt = df[filt]
-    grouped = pd.DataFrame(df_filt.groupby(by='airline')['airline_sentiment'].value_counts(normalize=True)*100)
-    grouped.reset_index(inplace=True)
-    grouped['proportion'] = round(grouped['proportion'], 2)
+        for list_ in lists_of_words:
+            for word in list_:
+                if 'http' in word or word.startswith("@") or word == 'RT':
+                    list_.remove(word)
         
-    insights_filt_pos = grouped['airline_sentiment'] == "positive"
-    df_insights = grouped[insights_filt_pos]
-    filt = df_insights['proportion'] == df_insights['proportion'].max()
-    name = df_insights[filt]["airline"].iloc[0]
-    value_pos = round(df_insights[filt]["proportion"].iloc[0], 2)
 
-    insights_filt_neg = grouped['airline_sentiment'] == "negative"
-    df_insights_neg = grouped[insights_filt_neg]
-    filt_neg = df_insights_neg['proportion'] == df_insights_neg['proportion'].max()
-    name_neg = df_insights_neg[filt_neg]["airline"].iloc[0]
-    value_neg = round(df_insights_neg[filt_neg]["proportion"].iloc[0], 2)
+        return words_neg, words_neu, words_pos
 
-    st.markdown(f"* {name_neg} has the highest proportion of negative tweets ({value_neg} %)")
-    st.markdown(f"* {name} has the highest proportion of positive tweets ({value_pos} %)")
+    words_neg, words_neu, words_pos = create_word_lists()
 
 
-    fig = px.bar(data_frame =grouped, 
-                    x='airline', y='proportion',
-                    
-                    facet_col='airline_sentiment',
-                    color='airline',
-                    labels={"airline_sentiment":"Tweets", 
-                    "proportion": "Proportion of Tweets (%)"})
-    st.plotly_chart(fig)
+    if st.sidebar.checkbox("Show Word Cloud", False):
+        st.subheader(f"Word Cloud for {sentiment_radio.capitalize()} Tweets")
+        wordcloud = WordCloud(stopwords=STOPWORDS,
+                                background_color = 'white',
+                                width=800,
+                                height=640,
+                                colormap='plasma'
+                                )
+        if sentiment_radio == 'negative':
+            wordcloud.generate(' '.join(words_neg))
+        elif sentiment_radio == 'neutral':
+            wordcloud.generate(' '.join(words_neu))
+        elif sentiment_radio == 'positive':
+            wordcloud.generate(' '.join(words_pos))
 
-st.sidebar.subheader("Word Cloud for Tweets")
-sentiment_radio = st.sidebar.radio("Choose Sentiment", 
-                tuple(df['airline_sentiment'].unique()))
-
-
-
-# for word in words:
-#     if 'http' in word or word.startswith("@") or word == 'RT':
-#         words.remove(word)
-
-@st.cache_data(persist=True)
-def split_df_by_sentiment():
-    filt_neg = df['airline_sentiment'] == 'negative'
-    filt_neu = df['airline_sentiment'] == 'neutral'
-    filt_pos = df['airline_sentiment'] == 'positive'
-
-    df_neg = df[filt_neg]
-    df_neu = df[filt_neu]
-    df_pos = df[filt_pos]
-
-    return df_neg, df_neu, df_pos
-
-df_neg, df_neu, df_pos = split_df_by_sentiment()
-
-@st.cache_data(persist=True)
-def create_word_lists():
-    sep = " "
-    
-    text_list_neg = df_neg['text'].tolist()
-    text_list_neu = df_neu['text'].tolist()
-    text_list_pos = df_pos['text'].tolist()
-
-    words_neg = sep.join(text_list_neg).split()
-    words_neu = sep.join(text_list_neu).split()
-    words_pos = sep.join(text_list_pos).split()
-
-    lists_of_words = [words_neg, words_neu, words_pos]
-
-
-    for list_ in lists_of_words:
-        for word in list_:
-            if 'http' in word or word.startswith("@") or word == 'RT':
-                list_.remove(word)
-    
-
-    return words_neg, words_neu, words_pos
-
-words_neg, words_neu, words_pos = create_word_lists()
-
-
-if st.sidebar.checkbox("Show Word Cloud", False):
-    st.subheader(f"Word Cloud for {sentiment_radio.capitalize()} Tweets")
-    wordcloud = WordCloud(stopwords=STOPWORDS,
-                            background_color = 'white',
-                            width=800,
-                            height=640,
-                            colormap='plasma'
-                            )
-    if sentiment_radio == 'negative':
-        wordcloud.generate(' '.join(words_neg))
-    elif sentiment_radio == 'neutral':
-        wordcloud.generate(' '.join(words_neu))
-    elif sentiment_radio == 'positive':
-        wordcloud.generate(' '.join(words_pos))
-
-    st.image(wordcloud.to_array())
-
+        st.image(wordcloud.to_array())
+except Exception as error:
+    st.error(f"An error occurred: {error}")
 
 # if st.sidebar.checkbox("Show Sample of Raw Data"):
 #     st.subheader("Raw Data")
